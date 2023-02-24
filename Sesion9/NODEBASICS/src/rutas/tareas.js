9d2f652d34ac5a8bac76dfe0eb171e4c20e39f54
humbertpa <humbertpal@gmail.com>
2023-02-24 15:01:06 -0600

    handlebars

--- Sesion9/NODEBASICS/src/rutas/tareas.js
+++ Sesion9/NODEBASICS/src/rutas/tareas.js
@@ -1,7 +1,26 @@
 const router = require('express').Router();
 
 router.get('',function (req,res) {
-    res.send('Listado de tareas ' + req.usuario);
+    //res.send('Listado de tareas ' + req.usuario);
+    const tareas = [
+        { titulo : 'Tarea 1' , status : 'nueva'},
+        { titulo : 'Tarea 2' , status : 'en progreso'},
+        { titulo : 'Tarea 3' , status : 'completa'},
+    ]
+
 })
+
+router.post('',function (req,res) {
+    console.log('Se crea la tarea ',req.body);
+    res.send('tarea creada correctamente');
+})
+
+router.get('/:id',function (req,res) {
+    const tarea = {
+        titulo : 'Tarea 1',
+        status : 'nueva',
+    }
+})
+
 
 module.exports = router;
