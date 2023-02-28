46ae5276cce8e754efa34751a2b7e752da81da5f
PEÑUELAS ALMEIDA <humbertpal@gmail.com>
2023-02-28 14:30:38 -0600

    ahora las rutas están separadas en sus propios archivos y carpetas

--- Tareas/Tarea3/rutas.js
+++ Tareas/Tarea3/rutas.js
@@ -6,34 +6,8 @@ module.exports = function (app) {
     app.use('/assets', express.static(path.join(__dirname,'/src/assets')));
 
     app.get('/',function (req,res) {
-        const ruta = path.join(__dirname,'src/views/index.html');
+        const ruta = path.join(__dirname,'src/views/indice.html');
         res.sendFile(ruta);
     })
 
-    app.get('/main',function (req,res) {
-        const ruta = path.join(__dirname,'src/views/main.html');
-        res.sendFile(ruta);
-    })
-
-    app.get('/fotos',function (req,res) {
-        const ruta = path.join(__dirname,'src/views/fotos.html');
-        res.sendFile(ruta);
-    })
-
-    app.get('/hobbies',function (req,res) {
-        const ruta = path.join(__dirname,'src/views/hobbies.html');
-        res.sendFile(ruta);
-    })
-    
-    app.get('/contacto',function (req,res) {
-        const ruta = path.join(__dirname,'src/views/contacto.html');
-        res.sendFile(ruta);
-    })
-
-    app.get("*", function (req, res) {
-        const ruta = path.join(__dirname,'src/views/noPagina.html');
-        res.status(404).sendFile(ruta);
-        //res.status(404).send("Pagina no encontrada");
-    }
-    );
 }
